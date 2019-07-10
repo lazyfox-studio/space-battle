3b2eb12125595fd81bda04991b7ab2dc9430655b
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2019-07-10 09:35:18 +0300

    Создан Component

--- src/Headers/Actor/Actor.h
+++ src/Headers/Actor/Actor.h
@@ -4,18 +4,18 @@
 #include <cmath>
 #include "Constants.h"
 #include "../Flipbook/Flipbook.h"
+#include"../Math/Component/Component.h"
+#include"../Math/Component/Rectangle/Rectangle.h"
+#include"../Math/Component/Ellipse/Ellipse.h"
 
 // Actor (объект, который может быть помещён на игровую локацию)
 class Actor
 {
 protected:
-    // Координаты
-    float x;
-    float y;
+    // Компонента тела
+    Component* component;
 
     // Параметры тела
-    float height; //Высота компоненты
-    float width;  //Ширина компоненты
     float m;      // Масса
 
     // Свойства тела
@@ -32,7 +32,7 @@ protected:
 public:
     //Конструкторы и Деструкторы
     Actor();
-    Actor(float x_, float y_, float height_, float widht_, float m_, bool attractable_, bool touchable_, const sf::Vector2f velocity_, const Flipbook sprite_);
+    Actor(float x_, float y_, float height_, float widht_, unsigned componentType, float m_, bool attractable_, bool touchable_, const sf::Vector2f velocity_, const Flipbook sprite_);
     Actor(const Actor& temp);
     ~Actor();
 
---
+++ src/Headers/Math/Component/Rectangle/Rectangle.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "../Component.h"
+
+class Rectangle : Component
+{
+
+};
