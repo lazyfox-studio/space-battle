509bbfb46371a64e609d34dbe41ffa695ba2d182
vlasov-unn <vla2000@inbox.ru>
2019-07-14 20:40:46 +0300

    тип float

--- src/Functions/Physics/Physics.h
+++ src/Functions/Physics/Physics.h
@@ -17,11 +17,11 @@ namespace Physics
 
 	// Расстояние
 	template<typename Type1, typename Type2>
-	void distance(const Type1& a, const Type2& b);
+	float distance(const Type1& a, const Type2& b);
 }
 
 template<typename Type1, typename Type2>
-void Physics::distance(const Type1& a, const Type2& b)
+float Physics::distance(const Type1& a, const Type2& b)
 {
 	float x1 = a.getX(), x2 = b.getX(), y1 = a.getY(), y2 = b.getY();
 	return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
